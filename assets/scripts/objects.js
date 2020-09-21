22cc80bb45c3dedae0aa398bd7d0303802e69c56
WoolKnitCap <woolknitcap@outlook.com>
2020-09-21 15:26:23 -0400

    Start!

---
+++ assets/scripts/objects.js
@@ -0,0 +1,13 @@
+let person = {
+  name: 'Max',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  greet: function() {
+    alert('Hi there!')l
+  }
+};
+
+// ...
+
+person.isAdmin = true;
+console.log(person.isAdmin);
